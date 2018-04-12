57c39780675c232a22294f50639e7133db6f8263
David Turner <Dturner753@yahoo.com>
2018-04-12 09:54:36 -0700

    react homework

---
+++ src/components/Navpills.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navpills = () => (
+  <ul className="nav nav-tabs">
+    <li>Clicky Game!</li>
+    <li>Make a guess!</li>
+    <li>Current Score: </li>
+    <li>High Score: </li>
+  </ul>
+);
+
+export default Navpills;
